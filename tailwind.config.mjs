5f3b3c7cb480938e7aad766ede26c5e726cbdd80
souichiroyamamura <yamamurasouichiro@gmail.com>
2024-07-10 15:38:27 +0900

    re build

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'mainFamily': ['Noto Sans JP', '-apple-system', '"Helvetica Neue"', '"游ゴシック体"', 'yugothic', '"游ゴシック"', '"Yu Gothic"', '"ヒラギノ角ゴ ProN W3"', 'hiragino kaku gothic pron', 'arial', '"メイリオ"', 'meiryo', 'sans-serif'],
+				'en': ['"Helvetica Neue"', 'helvetica', 'sans-serif'],
+			  },
+		},
 	},
 	plugins: [],
 }
